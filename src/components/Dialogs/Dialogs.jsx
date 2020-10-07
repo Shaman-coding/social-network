65a78db63ee6b4e4043acf03f8a89255deb80431
Kirill <kirya.zinovev.01@bk.ru>
2020-10-07 20:18:42 +0300

    Make section message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import P from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div >
-            Dialogs <br/>
+        <div className={P.Dialogs}>
+           <div className={P.Friend}>
+                <NavLink to='/Kirill' className={P.FriendsItem + " " + P.active}>
+                    Kirill
+                </NavLink>
+                <div className={P.FriendsItem}>
+                    Dasha
+                </div>
+                <div className={P.FriendsItem}>
+                    Maxim
+                </div>
+                <div className={P.FriendsItem}>
+                    Oleg
+                </div>
+                <NavLink to='/Pasha' >
+                    Pasha
+                </NavLink>
+           </div>
+           <div className='message'>
+               <div className={P.messageItem}>Hey,bro</div>
+               <div className={P.messageItem}>i'm good,you ?</div>
+               <div className={P.messageItem}>Nice,bro</div>
+               <div className={P.messageItem}>oo,men </div>
+               <div className={P.messageItem}>i too  </div>
+           </div>
            
         </div>
     )
